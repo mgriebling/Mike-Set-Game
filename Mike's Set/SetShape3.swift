1c7db4388d1ba3e5de39dabce78eb5985a28608c
Michael Griebling <sam@c-inspirations.com>
2020-09-23 06:30:15 -0400

    Added new shapes and stripe fill extension.

--- Mike's Set/SetShape3.swift
+++ Mike's Set/SetShape3.swift
@@ -28,21 +28,3 @@ struct SetShape3: Shape {
     
 }
 
-private struct Diamond : Shape {
-    
-    func path(in rect: CGRect) -> Path {
-        let start = CGPoint(x: rect.midX, y: rect.minY)
-        let right = CGPoint(x: rect.maxX, y: rect.midY)
-        let bottom = CGPoint(x: rect.midX, y: rect.maxY)
-        let left = CGPoint(x: rect.minX, y: rect.midY)
-        var p = Path()
-        p.move(to: start)
-        p.addLine(to: right)
-        p.addLine(to: bottom)
-        p.addLine(to: left)
-        p.addLine(to: start)
-        return p
-    }
-   
-}
-
--- Mike's Set/Shape3SetGameView.swift
+++ Mike's Set/Shape3SetGameView.swift
@@ -6,6 +6,47 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
+
+extension CGImage {
+    
+    static func generateStripePattern(
+        colors: (UIColor, UIColor) = (.clear, .black),
+        width: CGFloat = 6,
+        ratio: CGFloat = 1) -> CGImage? {
+        
+        let context = CIContext()
+        let stripes = CIFilter.stripesGenerator()
+        stripes.color0 = CIColor(color: colors.0)
+        stripes.color1 = CIColor(color: colors.1)
+        stripes.width = Float(width)
+        stripes.center = CGPoint(x: 1-width*ratio, y: 0)
+        let size = CGSize(width: width, height: 1)
+        
+        guard
+            let stripesImage = stripes.outputImage,
+            let image = context.createCGImage(stripesImage, from: CGRect(origin: .zero, size: size))
+        else { return nil }
+        return image
+    }
+    
+}
+
+extension Shape {
+
+    func stripes(angle: Double = 45, colour: Color = .black) -> AnyView {
+        let stripeColor = UIColor(colour)
+        guard
+            let stripePattern = CGImage.generateStripePattern(colors: (.clear, stripeColor))
+        else { return AnyView(self)}
+
+        return AnyView(Rectangle().fill(ImagePaint(
+            image: Image(decorative: stripePattern, scale: 1.0)))
+        .scaleEffect(2)
+        .rotationEffect(.degrees(angle))
+        .clipShape(self))
+    }
+}
 
 struct Shape3SetGameView: View {
     @ObservedObject var viewModel: Shape3SetGame
@@ -59,14 +100,19 @@ struct CardView: View {
         let shade = card.content.shade
         let height = size.height / 4
         let width = min(size.width, height*1.7)
-        print(size.height, size.width)
+//        print(size.height, size.width)
         return VStack() {
             ForEach(0 ..< number)  { id in
                 ZStack {
+                    if shade == .hatched {
+                        SetShape3(shape: card.content.shape)
+                            .stripes(angle: 0, colour: colour)
+                    } else {
+                        SetShape3(shape: card.content.shape)
+                            .fill(colour.opacity(shade == .none ? 0 : shade == .solid ? 1 : 0.3))
+                    }
                     SetShape3(shape: card.content.shape)
-                        .fill(colour.opacity(shade == .none ? 0 : shade == .solid ? 1 : 0.3))
-                    SetShape3(shape: card.content.shape)
-                        .stroke(colour, lineWidth: 2)
+                        .stroke(colour, lineWidth: 3)
                 }
                 .frame(width: width, height: height, alignment: .center)
             }
